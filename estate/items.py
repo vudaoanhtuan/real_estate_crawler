e5a1ff27ad3dc3841bc7c4718cc074f17c18c905
Vũ Đào Anh Tuấn <vudaoanhtuan@gmail.com>
2020-07-06 21:09:54 +0700

    fix strip data

--- estate/items.py
+++ estate/items.py
@@ -10,6 +10,11 @@ from scrapy.loader.processors import MapCompose, TakeFirst
 from datetime import datetime
 
 
+def strip_text(text):
+    try: text = str.strip(text)
+    else: pass
+    return text
+
 def convert_float(text):
     try: text = float(text)
     except: pass
@@ -22,38 +27,38 @@ def convert_int(text):
 
 
 class RealEstateItem(Item):
-    post_url = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
-    post_title = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
-    post_date = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
-    post_detail = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
+    post_url = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
+    post_title = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
+    post_date = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
+    post_detail = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
 
-    post_author_url = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
-    post_author_name = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())  
-    post_author_phone = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
+    post_author_url = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
+    post_author_name = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())  
+    post_author_phone = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
 
     post_images = Field()
 
-    re_type = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
-    re_price = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
+    re_type = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
+    re_price = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
 
     re_width = Field(input_processor=MapCompose(convert_float), output_processor=TakeFirst())
     re_length = Field(input_processor=MapCompose(convert_float), output_processor=TakeFirst())
     re_area = Field(input_processor=MapCompose(convert_float), output_processor=TakeFirst())
     re_area_to_use = Field(input_processor=MapCompose(convert_float), output_processor=TakeFirst())
 
-    re_direction = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
-    re_legal = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
+    re_direction = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
+    re_legal = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
 
     re_num_floors = Field(input_processor=MapCompose(convert_int), output_processor=TakeFirst())
     re_bathroom = Field(input_processor=MapCompose(convert_int), output_processor=TakeFirst())
     re_bedroom = Field(input_processor=MapCompose(convert_int), output_processor=TakeFirst())
     
-    re_addr_full = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
-    re_addr_city = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
-    re_addr_district = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
-    re_addr_ward = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
-    re_addr_street_name = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
-    re_addr_street_number = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
-    re_addr_building = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
+    re_addr_full = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
+    re_addr_city = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
+    re_addr_district = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
+    re_addr_ward = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
+    re_addr_street_name = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
+    re_addr_street_number = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
+    re_addr_building = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
 
-    re_agency = Field(input_processor=MapCompose(str.strip), output_processor=TakeFirst())
+    re_agency = Field(input_processor=MapCompose(strip_text), output_processor=TakeFirst())
